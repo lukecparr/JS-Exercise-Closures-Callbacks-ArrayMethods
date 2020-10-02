4b40f0683a6ba5b6e47c3b6772b1cbe6b887759d
Lukecp <lukecp@pm.me>
2020-10-02 10:43:32 -0400

    added some final adjustments and comments explaining code

--- index.js
+++ index.js
@@ -27,13 +27,13 @@ function processFirstItem(stringList, callback) {
  * Study the code for counter1 and counter2. Answer the questions below.
  * 
  * 1. What is the difference between counter1 and counter2?
- * Counter1's 'count' variable is function scoped and private to that fucntion while counter2's count variable is globally scoped and could be modified by any function. Also, counterMaker is missing the 'return' before 'count++'. Seems like a typo.
+ * Counter1 returns a function so it can be used and reused to create multiple counters. counter2 will return a total that will be added to each time the function is called. (Also, counterMaker is missing the 'return' before 'count++'. Seems like a typo.)
  
  * 2. Which of the two uses a closure? How can you tell?
- * counter2 uses a closure because it has to reach outside its own scope to provide "closure" for the variable 'count'. Although from what I know, counter1 is technically using a closure as well when it reaches for it's own 'count' variable and creates a closure.
+ * counter2 uses a closure because it has to reach outside its own scope to provide "closure" for the variable 'count'.
  
  * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better? 
- * counter1 would be preferable when you don't need the count variable to be accessible globally or by any other function. counter2 would be preferable if we do want count to be accissible globally, but we would have to be careful that we're not misusing the variable elseware so that it breaks something.
+ * counter1 is useful when you want to create new counters that will each start at 0 when called. counter2 is useful if you want to keep a running total of something, since the 'count' variable will be added to each time the function is called.
 */
 
 // counter1 code
@@ -59,12 +59,13 @@ function counter2() {
 Write a function called `inning` that generates a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
 
 function inning() {
-
+	
+	//generate then return a random whole number between 0 and 2
 	const inningScore = Math.floor(Math.random() * 3);
 	return inningScore;
 }
 
-//console.log(inning());
+console.log(inning());
 
 /* Task 3: finalScore()
 
@@ -81,21 +82,24 @@ finalScore(inning, 9) might return:
 */
 
 function finalScore(func, num) {
-
+	
+	//declare an array to hold the final scores
 	const final = {
 		home: 0,
 		away: 0,
 	};
-
+	
+	//for each inning, generate a score and add that to the total score for that team
 	for (num; num > 0; num--) {
 		final.home += func();
 		final.away += func();
 	};
-
+	
+	//return the final object with total scores
 	return final;
 }
 
-//console.log(finalScore(inning, 9));
+console.log(finalScore(inning, 9));
 
 /* Task 4: 
 
@@ -119,28 +123,36 @@ and returns the score at each pont in the game, like so:
 
 Final Score: awayTeam - homeTeam */
 
-function scoreboard(cbFinal, cbInning, num) {
 
+//The instruction above to pass in `getInningScore` doesn't make sense to me. That's not a function in this doc and we weren't askde to create it. I assumed it was some kind of mistake when updating the project. When invocing `scoreboard` below I passed in my `finalScore` function instead, but did not invoce it within the function as it's not needed.
+function scoreboard(cbFinal, cbInning, num) {
+	
+	//declare an object to hold final scores
 	const final = {
 		home: 0,
 		away: 0,
 	};
 
+	// for each inning, generate a random score for each team
 	for (let i = 0; i < num; i++) {
 		home = cbInning();
 		away = cbInning();
 
+		//console.log the scores with iterpolation
 		console.log(`Inning ${i+1}: ${away} - ${home}`)
 
+		//add the inning scores to the totals
 		final.home += home;
 		final.away += away;
 	}
-
+	
+	//console.log the final scores
 	console.log(`Final Score: ${final.away} - ${final.home}`)
 	
-//	console.log(cbFinal(cbInning, num))
+	
+	//I included this just as an example. I could call the finalScore function that I passed in to scoreboard, but if I did it would generate a whole NEW set of inning scores and totals rather than working off of what was created in this function.
+	//console.log(cbFinal(cbInning, num))
 }
-
 
 scoreboard(finalScore, inning, 9)
 
